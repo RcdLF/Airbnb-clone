419f15d0f5d466f2de60d1f96891bedbea59f5e0
unknown <luciofuentes55@gmail.com>
2023-07-07 06:59:59 -0300

    deploy

---
+++ app/actions/getListings.ts
@@ -0,0 +1,91 @@
+import prisma from "@/app/libs/prismadb";
+
+export interface IListingParams {
+  userId: string;
+  guestCount?: number;
+  roomCount?: number;
+  bathroomCounter?: number;
+  startDate?: string;
+  endDate?: string;
+  locationValue?: string;
+  category?: string;
+}
+
+export default async function getListings(params: IListingParams) {
+  try {
+    const {
+      userId,
+      guestCount,
+      roomCount,
+      bathroomCounter,
+      startDate,
+      endDate,
+      locationValue,
+      category,
+    } = params;
+
+    let query: any = {};
+
+    if (userId) {
+      query.userId = userId;
+    }
+
+    if (category) {
+      query.category = category;
+    }
+
+    if (roomCount) {
+      query.roomCount = {
+        gte: +roomCount,
+      };
+    }
+    if (guestCount) {
+      query.guestCount = {
+        gte: +guestCount,
+      };
+    }
+    if (bathroomCounter) {
+      query.bathroomCounter = {
+        gte: +bathroomCounter,
+      };
+    }
+
+    if (locationValue) {
+      query.locationValue = locationValue;
+    }
+
+    if (startDate && endDate) {
+      query.NOT = {
+        reservations: {
+          some: {
+            OR: [
+              {
+                endDate: { gte: startDate },
+                startDate: { lte: startDate },
+              },
+              {
+                startDate: { lte: endDate },
+                endDate: { gte: endDate },
+              },
+            ],
+          },
+        },
+      };
+    }
+
+    const listings = await prisma.listing.findMany({
+      where: query,
+      orderBy: {
+        createAt: "desc",
+      },
+    });
+
+    const safeListings = listings.map((listing) => ({
+      ...listing,
+      createAt: listing.createAt.toISOString(),
+    }));
+    return safeListings;
+  } catch (error: any) {
+    throw new Error(error);
+  }
+}
---
+++ app/properties/page.tsx
@@ -0,0 +1,49 @@
+import EmptyState from "../components/EmptyState";
+import ClientOnly from "../components/ClientOnly";
+
+import getCurrentUser from "../actions/getCurrentUser";
+import PropertiesClient from "./PropertiesClient";
+import getListings from "../actions/getListings";
+
+const PropertiesPage = async () => {
+    
+    const currentUser = await getCurrentUser();
+
+    if(!currentUser){
+        return (
+            <ClientOnly>
+                <EmptyState
+                    title="Unauthorized"
+                    subtitle="Please Login"
+                />
+            </ClientOnly>
+        )
+    }
+
+    const listing = await getListings({
+        userId : currentUser.id
+    });
+
+    if(listing.length === 0){
+        return (
+            <ClientOnly >
+                <EmptyState 
+                 title=" No properties found"
+                 subtitle="Looks like you have not properties any trips"
+                /> 
+            </ClientOnly>
+        )
+    }
+
+    return (
+        <ClientOnly>
+            <PropertiesClient 
+             listing={listing}
+             currentUser={currentUser}
+            />
+        </ClientOnly>
+    )
+
+}
+
+export default PropertiesPage
